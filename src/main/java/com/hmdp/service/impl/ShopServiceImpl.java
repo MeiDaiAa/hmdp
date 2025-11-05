1ccea9e98c2ac966c541d8b68e8459a3e81aab4f
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2025-11-05 11:30:48 +0800

    fix(shop): 修复店铺信息查询逻辑
    - 添加店铺ID空值校验
    - 处理缓存空值情况，避免缓存穿透
    - 数据库查询为空时，将空值写入缓存并设置较短过期时间

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -12,8 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -31,26 +31,35 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result getShopInfoById(Long id) {
+        // 0. 判空
+        if (id == null) {
+            return Result.fail("店铺id不能为空");
+        }
+        String key = CACHE_SHOP_KEY + id;
         // 1. 查看缓存中是否有商铺信息
-        String s = redisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        String s = redisTemplate.opsForValue().get(key);
         // 2.  缓存中有，返回
         if (s != null) {
+            // 缓存中为空，返回错误
+            if(StrUtil.isBlank(s)) {
+                return Result.fail("店铺不存在");
+            }
+            // 缓存中有，返回
             Shop bean = JSONUtil.toBean(s, Shop.class);
             return Result.ok(bean);
         }
         // 3. 缓存中没有，查询数据库
         Shop byId = getById(id);
-        // 4. 数据库中没有，返回错误
+        // 4. 数据库中没有，返回错误 (update: 数据库中没有，将空值写入缓存(有效时间2Min)，返回错误)
         if (byId == null) {
+            // 将空值写入缓存
+            redisTemplate.opsForValue()
+                    .set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在");
         }
         // 5. 数据库中有，写入缓存
         redisTemplate.opsForValue()
-                .set(CACHE_SHOP_KEY + id,
-                        JSONUtil.toJsonStr(byId),
-                        CACHE_SHOP_TTL,
-                        TimeUnit.MINUTES
-                );
+                .set(key, JSONUtil.toJsonStr(byId), CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 6. 返回
         return Result.ok(byId);
     }
