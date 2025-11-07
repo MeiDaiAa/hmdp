070cf5f6ce3316aa56230bbb561102e40813e671
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2025-11-07 13:53:18 +0800

    feat(voucher): 实现秒杀优惠券功能
    - 实现完整的秒杀流程、乐观锁解决超卖问题
    - 添加了时间校验、库存扣减和订单创建逻辑
    - 使用 Redis 生成唯一订单号
    - 加入事务管理确保操作原子性

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
+import com.hmdp.entity.Voucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IVoucherService;
+import com.hmdp.utils.RedisClient;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +27,53 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+    @Autowired
+    private ISeckillVoucherService seckillVoucherService;
+    @Autowired
+    private RedisClient redisClient;
 
+
+    /**
+     * 秒杀优惠券
+     * @param voucherId 优惠券id
+     * @return 订单id
+     */
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 秒杀优惠券
+        // 1. 查询优惠卷信息
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+        // 2. 查看时间是否过期
+        if (LocalDateTime.now().isBefore(seckillVoucher.getBeginTime())) {
+            return Result.fail("优惠券尚未开始");
+        }
+        if (LocalDateTime.now().isAfter(seckillVoucher.getEndTime())) {
+            return Result.fail("优惠券已过期");
+        }
+        // 3. 查看库存是否充足
+        Integer stock = seckillVoucher.getStock();
+        if (stock <= 0) {
+            return Result.fail("优惠券已售罄");
+        }
+        // 4. 扣减库存
+        boolean update = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)
+                .update();
+        if (!update) {
+            return Result.fail("优惠券已售罄");
+        }
+        // 5. 创建订单
+        long order = redisClient.getUniqueId("voucherOrder");
+        VoucherOrder voucherOrder = new VoucherOrder()
+                .setId(order)
+                .setUserId(UserHolder.getUser().getId())
+                .setVoucherId(voucherId);
+        // 6. 保存订单
+        save(voucherOrder);
+
+        return Result.ok(order);
+    }
 }
